00bbe08eddd03a2453912000258adf54ca873100
Anirudh Kumar <anirudhkumar790@gmail.com>
2019-08-13 19:42:26 +0530

    final commit

--- crud_ajax_modal/views.py
+++ crud_ajax_modal/views.py
@@ -1,42 +1,73 @@
-from django.shortcuts import render
-from .models import Book
+# from django.shortcuts import render
+# from .models import Book
+# from django.http import JsonResponse
+# from django.template.loader import render_to_string
+# from .forms import BookForm
+
+
+# def book_create(request):
+#     # form = BookForm()
+#     # context = {'form': form}
+#     # html_form = render_to_string('books/partial_book_create.html',
+#     #                              context,
+#     #                              request=request,
+#     #                              )
+#     # return JsonResponse({'html_form': html_form})
+
+#     # data = dict()
+
+#     # if request.method == 'POST':
+#     #     form = BookForm(request.POST)
+#     #     if form.is_valid():
+#     #         form.save()
+#     #         data['form_is_valid'] = True
+#     #     else:
+#     #         data['form_is_valid'] = False
+#     # else:
+#     #     form = BookForm()
+
+#     # context = {'form': form}
+#     # data['html_form'] = render_to_string('books/partial_book_create.html',
+#     #                                      context,
+#     #                                      request=request
+#     #                                      )
+#     # return JsonResponse(data)
+
+#     data = dict()
+
+#     if request.method == 'POST':
+#         form = BookForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             data['form_is_valid'] = True
+#             books = Book.objects.all()
+#             data['html_book_list'] = render_to_string('books/partial_book_list.html', {
+#                 'books': books
+#             })
+#         else:
+#             data['form_is_valid'] = False
+#     else:
+#         form = BookForm()
+
+#     context = {'form': form}
+#     data['html_form'] = render_to_string('books/partial_book_create.html',
+#                                          context,
+#                                          request=request
+#                                          )
+#     return JsonResponse(data)
+
+
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+
+from .models import Book
 from .forms import BookForm
 
 
-def book_create(request):
-    # form = BookForm()
-    # context = {'form': form}
-    # html_form = render_to_string('books/partial_book_create.html',
-    #                              context,
-    #                              request=request,
-    #                              )
-    # return JsonResponse({'html_form': html_form})
-
-    # data = dict()
-
-    # if request.method == 'POST':
-    #     form = BookForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         data['form_is_valid'] = True
-    #     else:
-    #         data['form_is_valid'] = False
-    # else:
-    #     form = BookForm()
-
-    # context = {'form': form}
-    # data['html_form'] = render_to_string('books/partial_book_create.html',
-    #                                      context,
-    #                                      request=request
-    #                                      )
-    # return JsonResponse(data)
-
+def save_book_form(request, form, template_name):
     data = dict()
-
     if request.method == 'POST':
-        form = BookForm(request.POST)
         if form.is_valid():
             form.save()
             data['form_is_valid'] = True
@@ -46,14 +77,46 @@ def book_create(request):
             })
         else:
             data['form_is_valid'] = False
+    context = {'form': form}
+    data['html_form'] = render_to_string(
+        template_name, context, request=request)
+    return JsonResponse(data)
+
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
     else:
         form = BookForm()
+    return save_book_form(request, form, 'books/partial_book_create.html')
 
-    context = {'form': form}
-    data['html_form'] = render_to_string('books/partial_book_create.html',
-                                         context,
-                                         request=request
-                                         )
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+    else:
+        form = BookForm(instance=book)
+    return save_book_form(request, form, 'books/partial_book_update.html')
+
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    data = dict()
+    if request.method == 'POST':
+        book.delete()
+        # This is just to play along with the existing code
+        data['form_is_valid'] = True
+        books = Book.objects.all()
+        data['html_book_list'] = render_to_string('books/partial_book_list.html', {
+            'books': books
+        })
+    else:
+        context = {'book': book}
+        data['html_form'] = render_to_string('books/partial_book_delete.html',
+                                             context,
+                                             request=request,
+                                             )
     return JsonResponse(data)
 
 
